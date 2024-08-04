31139f8eaad5dba77e6d83a2733c0f2e91605ffd
Patrik Gallik <patrik.gallik@gmail.com>
2024-08-04 15:51:16 +0200

    Add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.png" />
+      </head>
       <body>
         <div id="root">{children}</div>
       </body>
